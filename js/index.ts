763f01bc3903699180f2f4af7287fef5d171b706
Aiden Bai <aiden.bai05@gmail.com>
2022-02-24 15:49:50 -0800

    Fast update

--- js/index.ts
+++ js/index.ts
@@ -1,3 +1,5 @@
-const diff = new Date().getTime() - new Date('January 5, 2005').getTime();
-const age = (diff / 1000 / 60 / 60 / 24 / 365).toFixed(3);
-document.querySelector<HTMLElement>('#age').textContent = String(age);
+setInterval(() => {
+  const diff = new Date().getTime() - new Date('January 5, 2005').getTime();
+  const age = (diff / 1000 / 60 / 60 / 24 / 365).toFixed(9);
+  document.querySelector<HTMLElement>('#age').textContent = String(age);
+}, 1);
