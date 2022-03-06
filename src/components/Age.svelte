b5fdc78cb4b2bb8ade9ca06795dec2c740de95aa
Aiden Bai <aiden.bai05@gmail.com>
2022-03-06 12:40:27 -0800

    Fix astro fonts

--- src/components/Age.svelte
+++ src/components/Age.svelte
@@ -1,10 +1,9 @@
 <script>
   let age = 17;
-
   setInterval(() => {
     const diff = new Date().getTime() - new Date('January 5, 2005').getTime();
     age = (diff / 1000 / 60 / 60 / 24 / 365).toFixed(9);
-  }, 10);
+  }, 10)
 </script>
 
-{age}
+<strong>{age}</strong>
