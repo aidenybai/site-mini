c033e8dfcebd78ae6feb2f94fe9583e3d9514ad4
Aiden Bai <aiden.bai05@gmail.com>
2022-04-19 23:51:59 -0700

    fmt

--- src/components/Age.jsx
+++ src/components/Age.jsx
@@ -4,14 +4,16 @@ const BIRTHDAY = new Date('Janurary 5, 2005').getTime();
 
 export default function Age({ init }) {
   const [age, setAge] = createSignal(init);
-
-  setInterval(() => {
-    const diff = new Date().getTime() - BIRTHDAY;
-    setAge((diff / 1000 / 60 / 60 / 24 / 365).toFixed(9));
-  }, 10);
-
+  const [currInterval, setCurrInterval] = createSignal(null);
+  const createInterval = () => {
+    return setInterval(() => {
+      const diff = new Date().getTime() - BIRTHDAY;
+      setAge((diff / 1000 / 60 / 60 / 24 / 365).toFixed(9));
+    }, 10);
+  };
+  setCurrInterval(createInterval());
   return (
-    <strong>
+    <strong onMouseEnter={() => clearInterval(currInterval())} onMouseLeave={() => setCurrInterval(createInterval())}>
       <code>{age()}</code>
     </strong>
   );
